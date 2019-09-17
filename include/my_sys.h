e377f651e9622e49d502817631e726018f197225
Daniel Black <daniel@linux.ibm.com>
2019-09-17 15:28:47 +1000

    MDEV-18851: mysys copy win32 large page alloc/free from innodb
    To preserve the same allocation behavior on Windows we enable
    HAVE_LARGE_PAGE_OPTION for Windows.

--- include/my_sys.h
+++ include/my_sys.h
@@ -177,7 +177,7 @@ extern void *my_memdup(const void *from,size_t length,myf MyFlags);
 extern char *my_strdup(const char *from,myf MyFlags);
 extern char *my_strndup(const char *from, size_t length, myf MyFlags);
 
-#ifdef HAVE_LINUX_LARGE_PAGES
+#ifdef HAVE_LARGE_PAGE_OPTION
 extern uint my_get_large_page_size(void);
 extern uchar * my_large_malloc(size_t size, myf my_flags);
 extern void my_large_free(uchar *ptr);
@@ -185,7 +185,7 @@ extern void my_large_free(uchar *ptr);
 #define my_get_large_page_size() (0)
 #define my_large_malloc(A,B) my_malloc_lock((A),(B))
 #define my_large_free(A) my_free_lock((A))
-#endif /* HAVE_LINUX_LARGE_PAGES */
+#endif /* HAVE_LARGE_PAGE_OPTION */
 
 void my_init_atomic_write(void);
 #ifdef __linux__
